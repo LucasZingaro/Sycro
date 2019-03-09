99566a144ed91f153c0e5eaa1eeb1383b1439275
Lucas Zingaro <37312287+LucasZingaro@users.noreply.github.com>
2019-03-09 18:51:36 -0300

    Sycro 1.0
    Primeira versão.

---
+++ src/sycro/CopyArq.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sycro;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Lucas Zingaro
+ * @author baseado em diego_qmota Access in 08-03-2019
+ * (https://www.guj.com.br/t/swing-copiar-pasta-de-um-local-e-salvar-em-outra/132165/6)
+ */
+public class CopyArq {
+
+    public CopyArq() {
+    }
+
+    /**
+     * Define a lista de extensões ignoradas
+     *
+     * @param listExt - Lista de extensões ignoradas
+     */
+    public CopyArq(String[] listExt) {
+        this.listExt = listExt.clone();
+    }
+
+    /**
+     * Lista de extensões ignoradas
+     */
+    private String listExt[];
+
+    /**
+     * Pegar lista de extensões ignoradas
+     *
+     * @return Array com lista de extenções ignoradas
+     */
+    public String[] getListExt() {
+        return listExt.clone();
+    }
+
+    /**
+     * Atribuir lista de extensões ignoradas
+     *
+     * @param listExt - Array de extensões ignoradas
+     */
+    public void setListExt(String[] listExt) {
+        this.listExt = listExt.clone();
+    }
+
+    /**
+     * Faz a cópia de um único arquivo de uma origem para um destino. (Desde que
+     * não tenha uma extensão da lista de extensões ignoradas)
+     *
+     * @param fileIn - Arquivo de origem
+     * @param fileOut - Arquivo de destino
+     * @param fileOverwrite - Confirmação para sobrescrever os arquivos
+     * @throws IOException
+     */
+    public void copiarArquivo(File fileIn, File fileOut, boolean fileOverwrite) throws IOException {
+        if (fileOut.exists() && !fileOverwrite) {
+            return;
+        }
+        FileInputStream source = new FileInputStream(fileIn);
+        FileOutputStream destination = new FileOutputStream(fileOut);
+        FileChannel sourceFileChannel = source.getChannel();
+        FileChannel destinationFileChannel = destination.getChannel();
+        long size = sourceFileChannel.size();
+        if (listExt.length >= 1) {
+            for (String ext : listExt) {
+                if (!fileIn.getName().endsWith(ext)) {
+                    sourceFileChannel.transferTo(0, size, destinationFileChannel);
+                }
+            }
+        } else {
+            sourceFileChannel.transferTo(0, size, destinationFileChannel);
+        }
+        System.out.println("file=" + fileIn.getName());
+
+    }
+
+    /**
+     * Faz a cópia de uma pasta de uma origem para um destino. (Desde que não
+     * tenha uma extensão da lista de extensões ignoradas)
+     *
+     * @param directoryIn - Diretório onde estão os arquivos a serem copiados
+     * @param directoryOut - Diretório onde os arquivos serão copiados
+     * @param fileOverwrite - Confirmação para sobrescrever os arquivos
+     * @throws IOException, UnsupportedOperationException
+     */
+    public void copiarPasta(File directoryIn, File directoryOut, boolean fileOverwrite) throws IOException, UnsupportedOperationException {
+        if (!directoryOut.exists()) {
+            directoryOut.mkdir();
+        }
+        if (!directoryOut.isDirectory()) {
+            throw new UnsupportedOperationException("Destino deve ser um diretório");
+        }
+        if (!directoryIn.isDirectory()) {
+            copiarArquivo(directoryIn, directoryOut, fileOverwrite);
+        } else {
+            //caso de pastas
+            File[] files = directoryIn.listFiles();
+            for (int i = 0; i < files.length; ++i) {
+                if (files[i].isDirectory()) {
+                    copiarPasta(files[i], new File(directoryOut + "\\" + files[i].getName()), fileOverwrite);
+                } else {
+                    copiarArquivo(files[i], new File(directoryOut + "\\" + files[i].getName()), fileOverwrite);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Retorna o  tamanho da pasta desejada
+     * @param path - Path da pasta designada
+     * @return Tamanho da pasta em bytes
+     */
+    public static int getFolderSize(String path){
+        File folder = new File(path);
+        int size=0;
+        if(folder.isDirectory()){
+            String[] dirList = folder.list();
+            if(dirList!=null){
+                for (int i = 0; i < dirList.length; i++) {
+                    File f= new File(folder, dirList[i]);
+                    if(f.isDirectory()){
+                        String filePath = f.getPath();
+                        size+=getFolderSize(filePath);
+                        continue;
+                    }
+                    size+=f.length();
+                }
+            }
+        }
+        return size;
+    }
+}
---
+++ src/sycro/Sycro.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sycro;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JProgressBar;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Sycro {
+
+    protected File origem, destino;
+    private Timer timer;
+    private boolean fileOverwrite;
+    private final int delay=0;
+    private int intervalo=5000;
+    private CopyArq cpa;
+    public Sycro() {
+        
+    }
+    
+    /**
+     * Define e inicia o timer responsavél pela sincronização das pastas
+     * @param listExt - Lista de extensões de arquivos ignorados
+     * @param fileOverwrite - Confirmação de sobrescriçao de arquivos
+     * @param intervalo -Intervalo de tempo do timer em milesegundos
+     */
+    public void setCopiaTimer(String[] listExt,boolean fileOverwrite, int intervalo) {
+        this.fileOverwrite=fileOverwrite;
+        this.intervalo=intervalo;
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    cpa = new CopyArq(listExt);
+                    cpa.copiarPasta(origem, destino, fileOverwrite);
+                } catch (IOException | UnsupportedOperationException ex) {
+                    Logger.getLogger(Sycro.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println(".run()");
+            }
+        }, delay, this.intervalo);
+    } 
+    
+    /**
+    * Reinicia o timer já definido, adiantando sua execução 
+    */
+    public void restart(){
+        stop();
+        setCopiaTimer(cpa.getListExt(), fileOverwrite, intervalo);
+        
+    }
+    /**
+     * Encerra o timer
+     */
+    public void stop(){
+        timer.cancel();
+        timer.purge();
+        System.out.println("Fim");
+    }
+}
